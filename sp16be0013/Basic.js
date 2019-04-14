8c6f980f31ce3bb1a0c0dc8843e7142e57f44772
MAJU STUDENT <majustudent@jinnah.edu>
2019-04-14 15:55:01 +0500

    Exam

---
+++ sp16be0013/Basic.js
@@ -0,0 +1,14 @@
+import React from "react";
+import React, {Component} from "react";
+import {Header} from "./components/Header";
+import {Platform, StyleSheet, Text, View} from "react-native";
+
+
+class Basic extends Component {
+    render() {
+        return (
+                <Header/>
+            );
+        }
+      }
+      export default Basic;
